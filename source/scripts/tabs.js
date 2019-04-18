99c66fbeb9b3f367bd540e60a17e80045eb57070
loremlipsum <lipsum.webdevelop@gmail.com>
2019-04-18 16:37:51 +0300

    'lipsum'

---
+++ source/scripts/tabs.js
@@ -0,0 +1,27 @@
+'use strict';
+
+(function() {
+
+  var tabs = document.querySelector('.js-tabs');
+  var btns = tabs.querySelectorAll('.js-tabs-button');
+  var contents = tabs.querySelectorAll('.js-tabs-content');
+
+  for (var i = 0; i < btns.length; i++) {
+    btns[i].addEventListener('click', function (e) {
+      e.preventDefault();
+      var tab = e.currentTarget;
+      var atr = tab.getAttribute('data-tab');
+
+      for (var j = 0; j < contents.length; j++) {
+        contents[j].classList.remove('active');
+      };
+
+      for (var j = 0; j < btns.length; j++) {
+        btns[j].parentNode.classList.remove('active');
+      };
+
+      tab.parentNode.classList.add('active');
+      document.querySelector("." + atr).classList.add('active');
+    });
+  }
+})();
